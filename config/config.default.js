5af613000388afdfcff3390a8a9f25d8e0df22e8
army8735 <army8735@qq.com>
2018-08-31 13:06:50 +0800

    init

--- config/config.default.js
+++ config/config.default.js
@@ -6,6 +6,12 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1533449579600_6240';
 
+  config.session = {
+    key: 'sessionid',
+    maxAge: 7 * 24 * 3600 * 1000,
+    domain: 'ciyuan.site',
+  };
+
   // add your config here
   config.middleware = [];
 
