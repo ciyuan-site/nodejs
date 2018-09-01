337f0e9da3841dc418fc28d8cd7be2203ecef002
army8735 <army8735@qq.com>
2018-09-01 21:57:51 +0800

    init

--- app/assets/index/Home.jsx
+++ app/assets/index/Home.jsx
@@ -5,7 +5,10 @@ class Home extends migi.Component {
     super(...data);
   }
   render() {
-    return <div class="home">123</div>;
+    return <div class="home">
+      <img src="//zhuanquan.xin/ciyuan/03554fe81164cf7cb53a6329c64eba4a.png"/>
+      <p>沟通创作的次元~</p>
+    </div>;
   }
 }
 
